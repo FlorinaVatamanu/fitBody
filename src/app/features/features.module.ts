069723f20f537b766f635c24db0388318c58d842
FlorinaVatamanu <flory_0004@yahoo.com>
2022-05-21 17:09:31 +0300

    -Home page design

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { FeaturesComponent } from './features.component';
 import { CommonModule } from '@angular/common';
 import { FeaturesRoutingModule } from './features-routing.module';
+import { TopMenuModule } from './components/top-menu/top-menu.module';
 
 @NgModule({
   declarations: [
-    FeaturesComponent
+    FeaturesComponent,
   ],
   imports: [
     CommonModule,
-    FeaturesRoutingModule
+    FeaturesRoutingModule,
+    TopMenuModule
   ]
 })
 
--- src/app/features/home-page/home-page.module.ts
+++ src/app/features/home-page/home-page.module.ts
@@ -3,6 +3,7 @@ import { HomePageComponent } from './home-page.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HomePageRoutingModule } from './home-page-routing.module';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { HomePageRoutingModule } from './home-page-routing.module';
   imports: [
     CommonModule,
     FormsModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    ButtonModule
   ]
 })
 
